514492c0f3b5d04d60d8f293d154906992cca044
John Angelo Basilio <111488301+monozi@users.noreply.github.com>
2022-10-17 15:07:37 +0800

    Create App.java

---
+++ arrays/App.java
@@ -0,0 +1,58 @@
+public class App {
+    public static void main(String[] args) {
+
+        // // Array of Strings
+        // String[] myfavefastfood = { "Jollibee", "Mcdonalds", "KFC", "MangInasal",
+        //         "Chowking" };
+
+        // // Array of Integers
+        // int[] primeNumbers = { 2, 3, 5, 7, 9 };
+
+        //        // Access first array element
+        //        System.out.println(primeNumbers[0]);
+
+        //        // Access second array element
+        //        System.out.println(primeNumbers[1]);
+       
+        //        // Access third array element
+        //        System.out.println(primeNumbers[2]);
+       
+        //        // Access fourth array element
+        //        System.out.println(primeNumbers[3]);
+       
+        //        // Access last array element
+        //        System.out.println(primeNumbers[4]);
+
+        // Array of Characters
+        char[] myName = { 'A', 'N', 'G', 'E', 'L', 'O' };
+
+                    //   // Access first array element
+                    //   System.out.println(myName[0]);
+
+                    //   // Access second array element
+                    //   System.out.println(myName[1]);
+              
+                    //   // Access third array element
+                    //   System.out.println(myName[2]);
+              
+                    //   // Access fourth array element
+                    //   System.out.println(myName[3]);
+              
+                    //   // Access fifth array element
+                    //   System.out.println(myName[4]);
+
+                    //   // Access last array element
+                    //   System.out.println(myName[5]);
+
+                    // for (int counter = 0; counter < myName.length; counter ++) {
+                        
+                    //     if (myName[counter] == 'A' || myName[counter] == 'E' || myName[counter] == 'I' || 
+                    //     myName[counter] == 'O' || myName[counter] == 'U') 
+                    //     System.out.println(myName[counter]);
+                    // }
+                    
+                    for (int counter = 5; counter >= 0; counter--) {
+                        System.out.println(myName[counter]);
+                    }
+    }
+}
