6624994b60d7194067c3d661427df3890085f070
John Angelo Basilio <111488301+monozi@users.noreply.github.com>
2022-10-20 15:44:39 +0800

    Create array3x3.java

---
+++ arrays/array3x3.java
@@ -0,0 +1,53 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+       
+
+        // char[] myFirstname = {'J', 'O', 'H', 'N'};
+
+        // for (int i = 0; i < myFirstname.length; i ++) {
+        //     // System.out.print(myFirstname[i]);
+        
+        //     if (!(myFirstname[i] == 'A' || myFirstname[i] == 'E' || myFirstname[i] == 'I' || 
+        //     myFirstname[i] == 'O' || myFirstname[i] == 'U')) {
+
+        //         System.out.println(myFirstname[i]);
+        //     }
+            
+            
+        
+        String[][] Array_3x3 = {
+            {" A", " B", " C"},
+            {" D", " E", " F"},
+            {" G", " H", " I"}
+
+        };
+        // System.out.println(Array_3x3[1][1]);
+        // System.out.println(Array_3x3[2][0]);
+        // System.out.println(Array_3x3[0][1]);
+        
+
+        for (int row = 0; row < Array_3x3.length; row++) {
+
+            for (int column = 0; column < Array_3x3.length; column ++) {
+                System.out.print(Array_3x3[row][column]);
+            }
+            System.out.println(" ");
+        }
+
+// for (int hours = 0; hours < 12; hours++) {
+
+
+//     for (int minutes = 0; minutes < 60; minutes ++) {
+
+//        for (int seconds = 0; seconds < 60; seconds ++)  
+//         System.out.println(hours + " : " + minutes + " : " + seconds);
+            
+//     }
+        
+// }
+
+        
+
+        }
+    }
+
